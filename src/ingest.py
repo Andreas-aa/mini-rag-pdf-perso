3a0dd14b37f9690e6c59f2fe8651bd47174c4eea
Andreas-aa <andreas.abromont@gmail.com>
2026-03-16 21:03:28 +0100

    requirements changed

--- src/ingest.py
+++ src/ingest.py
@@ -2,7 +2,7 @@ import os
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 DATA_PATH = "data"
 INDEX_PATH = "faiss_index"
